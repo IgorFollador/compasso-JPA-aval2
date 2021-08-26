79f79686c17a68c5e7ae288f6ebaf47284287d00
IgorFollador <igorledf@hotmail.com>
2021-08-26 17:39:51 -0300

    Corrigindo exercício número três

--- src/main/java/br/com/compasso/avaliacao/executa/ExecutaSentimentoEmMensagem.java
+++ src/main/java/br/com/compasso/avaliacao/executa/ExecutaSentimentoEmMensagem.java
@@ -7,37 +7,36 @@ import java.util.Scanner;
 public class ExecutaSentimentoEmMensagem {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
-			while(true) {
+			while (true) {
 				System.out.print("Entrada: ");
 				String msg = input.nextLine(), msgCopy = msg;
 				String feliz = ":-)", triste = ":-(";
 				int nFeliz = 0, nTriste = 0, aux;
 				
-				for(int i=0;i<msg.length();i++){
-					if(msg.contains(triste)){
-						aux = msg.indexOf(triste);
-						while(aux!=-1) {
-							msg = msg.substring(aux+3);
-							nTriste++;
-							aux = msg.indexOf(triste);
-						}
-					}
-					
-					if(msgCopy.contains(feliz)){
-						aux = msgCopy.indexOf(feliz);
-						while(aux!=-1) {
-							msgCopy = msgCopy.substring(aux+3);;
-							nFeliz++;
-							aux = msgCopy.indexOf(feliz);
-						}					
-					} 
-					
+				//Realizando contagem de emoticons tristes
+				aux = msg.indexOf(triste);
+				while (aux != -1) {
+					msg = msg.substring(aux + 3);
+					nTriste++;
+					aux = msg.indexOf(triste);
 				}
 				
+				//Realizando contagem de emoticons felizes
+				aux = msgCopy.indexOf(feliz);
+				while (aux != -1) {
+					msgCopy = msgCopy.substring(aux + 3);
+					;
+					nFeliz++;
+					aux = msgCopy.indexOf(feliz);
+				}
+
 				System.out.print("Saída: ");
-				if(nFeliz > nTriste) System.out.println("divertido");
-				else if(nFeliz < nTriste) System.out.println("chateado");
-				else System.out.println("neutro");
+				if (nFeliz > nTriste)
+					System.out.println("divertido");
+				else if (nFeliz < nTriste)
+					System.out.println("chateado");
+				else
+					System.out.println("neutro");
 			}
 		}
 	}
